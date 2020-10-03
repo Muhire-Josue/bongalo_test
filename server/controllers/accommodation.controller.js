a65e9e8b50fa3cf2cc805df31a7733139991a015
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-10-03 23:47:56 +0200

    remove yarn.lock

--- server/controllers/accommodation.controller.js
+++ server/controllers/accommodation.controller.js
@@ -8,23 +8,21 @@ const { successResponse } = handleResponse;
 const { created } = statusCode;
 const { accommodationCreated } = customMessage;
 
-
-
 /**
  * Accommodation controller
  */
 
- export default class AccommodationController{
+export default class AccommodationController {
 
     /**
      * save new accommodation
      */
 
-     static async createAccommodation(req, res){
-         const accommodation = req.body;
-         const { id } = req.authUser;
-         accommodation.userId = id;
-         const savedAccommodation = await save(accommodation);
-         return successResponse(res, created, accommodationCreated, undefined, savedAccommodation);
-     }
- }
+    static async createAccommodation(req, res) {
+        const accommodation = req.body;
+        const { id } = req.authUser;
+        accommodation.userId = id;
+        const savedAccommodation = await save(accommodation);
+        return successResponse(res, created, accommodationCreated, undefined, savedAccommodation);
+    }
+}
